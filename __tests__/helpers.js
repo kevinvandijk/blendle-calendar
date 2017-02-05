29cc3a2fb26049a910855084b36f16fc37d804b9
Kevin van Dijk <kevin@edgybits.com>
2017-02-05 02:26:46 +0100

    Sort expectations in tests for better readability

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,21 +1,23 @@
 import * as helpers from '../app/helpers';
 
 describe('Helpers', () => {
-  describe('#formatHours', () => {
-    it('formats a number of hours into an hour representation, starting from 00:00', () => {
-      const result1 = helpers.formatHours(0);
-      expect(result1).toBe('00:00');
+  describe('Formatters', () => {
+    describe('#formatHours', () => {
+      it('formats a number of hours into an hour representation, starting from 00:00', () => {
+        const result1 = helpers.formatHours(0);
+        const result2 = helpers.formatHours(4);
 
-      const result2 = helpers.formatHours(4);
-      expect(result2).toBe('04:00');
-    });
+        expect(result1).toBe('00:00');
+        expect(result2).toBe('04:00');
+      });
 
-    it('should be able to work with numbers over 24 and convert them into the right time', () => {
-      const result1 = helpers.formatHours(25);
-      expect(result1).toBe('01:00');
+      it('should be able to work with numbers over 24 and convert them into the right time', () => {
+        const result1 = helpers.formatHours(25);
+        const result2 = helpers.formatHours(34);
 
-      const result2 = helpers.formatHours(34);
-      expect(result2).toBe('10:00');
+        expect(result1).toBe('01:00');
+        expect(result2).toBe('10:00');
+      });
     });
   });
 });
