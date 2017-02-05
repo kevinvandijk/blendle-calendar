54f02d4103da7723bd83446b0ea7036f81ae6c8e
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 00:54:01 +0100

    Remove accidentally left in console.log

--- app/components/DailyEvents/Appointment.js
+++ app/components/DailyEvents/Appointment.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // h-calendar 2.0 compatible
 const Appointment = (props) => {
-  console.log('props', props.left);
   return (
     <dl className="DailyEvents-Appointment h-event" style={{ width: `${props.width}%`, height: `${props.height}%`, top: `${props.top}%`, left: `${props.left}%` }}>
       <dt className="DailyEvents-Appointment-time"><time className="dt-start" dateTime="2013-06-30 18:00"></time>-<time className="dt-end" dateTime=""></time></dt>
