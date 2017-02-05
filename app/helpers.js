84ba5b89071acbc7544cc4c175c179f8f7fecd10
Kevin van Dijk <kevin@edgybits.com>
2017-02-05 03:11:42 +0100

    Make hour proptype validator chainable

--- app/helpers.js
+++ app/helpers.js
@@ -1,7 +1,29 @@
 import { PropTypes as ReactPropTypes } from 'react';
 
-// Formatters
+// Source: https://github.com/facebook/react/issues/1715#issuecomment-168943070
+const chainablePropType = (predicate) => {
+  const propType = (props, propName, componentName) => {
+    // don't do any validation if empty
+    if (props[propName] == null) {
+      return null;
+    }
 
+    return predicate(props, propName, componentName);
+  }
+
+  propType.isRequired = (props, propName, componentName) => {
+    // warn if empty
+    if (props[propName] == null) {
+      return new Error(`Required prop \`${propName}\` was not specified in \`${componentName}\`.`);
+    }
+
+    return predicate(props, propName, componentName);
+  }
+
+  return propType;
+}
+
+// Formatters
 export function formatHours(amountOfHours) {
   const hour = (amountOfHours < 24
     ? amountOfHours
@@ -14,11 +36,11 @@ export function formatHours(amountOfHours) {
 // Custom PropTypes, not chainable at the moment
 export const PropTypes = {
   ...ReactPropTypes,
-  hour(props, propName) {
+  hour: chainablePropType((props, propName) => {
     if (props[propName] < 0 || props[propName] > 24) {
       return new Error(`\`${propName}\` should be a valid hour`);
     }
 
     return null;
-  }
+  })
 };
