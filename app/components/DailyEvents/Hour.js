1fe3d4bfe0e2a00394dc942d96103d29a51a84ec
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 00:47:49 +0100

    Allow Hour component to render children so Appointments can be put in hour slots

--- app/components/DailyEvents/Hour.js
+++ app/components/DailyEvents/Hour.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { PropTypes } from '../../helpers';
 
+const { hour, node } = PropTypes;
+
 const Hour = (props) => {
   return (
-    <li className="DailyEvents-hour">
+    <li className="DailyEvents-Hour">
       <time>{ props.label }</time>
+      <div>
+        { props.children }
+      </div>
     </li>
   );
 };
 
 Hour.propTypes = {
-  label: PropTypes.hour.isRequired
+  label: hour.isRequired,
+  children: node
 };
 
 export default Hour;
