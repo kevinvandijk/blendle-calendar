877253a788f7a94fe8870311c638eabf623431d9
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 18:40:36 +0100

    Add context to Form component so it can get and set values for children components

---
+++ app/components/Form/__tests__/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Form from '..';
+
+function createMockInputComponent(name, value) {
+  return {
+    props: {
+      name
+    },
+    getValue() {
+      return value;
+    }
+  };
+}
+
+describe('Components', () => {
+  describe('Form', () => {
+    it('Renders itself with children', () => {
+      const tree = renderer.create(
+        <Form>
+          <div>Form children woot</div>
+        </Form>
+      ).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Provides a childContext to attach input fields to and control the form with', () => {
+      // just a sanity check so that childContext isn't accidentally deleted this would potentially
+      // cause silent errors since InputFields are flexible and work without context as well
+      const expected = ['attach', 'detach', 'reset', 'submit'].sort();
+      expect(Object.keys(Form.childContextTypes).sort()).toEqual(expected);
+    });
+
+    it('Attaches components and is able to return their values, sorted by name', () => {
+      const name1 = 'A component has no name';
+      const name2 = 'A component always pays its debts'
+      const value = 'Value is coming';
+      const instance = shallow(<Form />).instance();
+
+      instance.attach(createMockInputComponent(name1, value));
+      instance.attach(createMockInputComponent(name2, value));
+
+      const result = instance.getValues();
+
+      expect(result).toEqual({
+        [name1]: value,
+        [name2]: value
+      });
+    });
+
+    it('Detaches components and does not include their values anymore in the return value', () => {
+      const mockComponent = createMockInputComponent('bla');
+      const instance = shallow(<Form />).instance();
+
+      instance.attach(mockComponent);
+      instance.detach(mockComponent);
+
+      const result = instance.getValues();
+
+      expect(result).toEqual({});
+    });
+
+    it('Calls a given onSubmit handler on submit with the collected values', () => {
+      const name = 'A component has no name';
+      const value = 'Value is coming';
+      const mockComponent = createMockInputComponent(name, value);
+      const submitStub = jest.fn();
+
+      const instance = shallow(<Form onSubmit={ submitStub } />).instance();
+      instance.attach(mockComponent);
+      instance.submit();
+
+      expect(submitStub).toHaveBeenCalledWith({
+        [name]: value
+      });
+    });
+
+    it('Sets the values of attached input fields to an empty string', () => {
+      const instance = shallow(<Form />).instance();
+      const stub1 = jest.fn();
+      const stub2 = jest.fn();
+      const mock1 = createMockInputComponent('bla', 'something');
+      const mock2 = createMockInputComponent('blabla', 'something else');
+
+      mock1.setValue = stub1;
+      mock2.setValue = stub2;
+
+      instance.attach(mock1);
+      instance.attach(mock2);
+      instance.reset();
+
+      expect(stub1).toHaveBeenCalledWith('');
+      expect(stub2).toHaveBeenCalledWith('');
+    });
+  });
+});
--- app/components/Form/index.js
+++ app/components/Form/index.js
@@ -2,18 +2,72 @@ import React from 'react';
 import './styles.scss';
 import { PropTypes } from '../../helpers';
 
-const { node } = PropTypes;
+const { node, func } = PropTypes;
 
-const Form = (props) => {
-  return (
-    <div>
-      { props.children }
-    </div>
-  );
-};
+class Form extends React.Component {
+  static propTypes = {
+    children: node,
+    onSubmit: func
+  }
 
-Form.propTypes = {
-  children: node
-};
+  static childContextTypes = {
+    attach: func,
+    detach: func,
+    submit: func,
+    reset: func
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.inputs = {};
+  }
+
+  getChildContext() {
+    return {
+      attach: this.attach,
+      detach: this.detach,
+      submit: this.submit,
+      reset: this.reset
+    };
+  }
+
+  getValues() {
+    return Object.keys(this.inputs).reduce((values, name) => {
+      return {
+        ...values,
+        [name]: this.inputs[name].getValue()
+      };
+    }, {});
+  }
+
+  attach = (component) => {
+    this.inputs[component.props.name] = component;
+  }
+
+  detach = (component) => {
+    delete this.inputs[component.props.name];
+  }
+
+  submit = () => {
+    const values = this.getValues();
+
+    if (this.props.onSubmit) this.props.onSubmit(values);
+  }
+
+  reset = () => {
+    Object.keys(this.inputs).forEach((name) => {
+      this.inputs[name].setValue('');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        { this.props.children }
+      </div>
+    );
+  }
+}
 
 export default Form;
