6fda9ceab306fc59b1acbb5b16595d893a6c8215
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 22:42:09 +0100

    Add redux into the mix

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
+import reducer from './reducer';
 import Main from './components/Main';
-
 import './styles.scss';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Main />,
+  <Provider store={ store }>
+    <Main />
+  </Provider>,
   document.getElementById('root')
 );
