433cb2cce59b1ee385ba4715f7780ec40ec23497
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 02:01:48 +0100

    Reorganize Appointment component for better readability

--- app/components/DailyEvents/Appointment.js
+++ app/components/DailyEvents/Appointment.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import moment from 'moment';
+import { PropTypes } from '../../helpers';
+
+const { number, string } = PropTypes;
 
 // h-calendar 2.0 compatible
 const Appointment = (props) => {
+  const appointmentStyle = {
+    width: `${props.width}%`,
+    height: `${props.height}%`,
+    top: `${props.top}%`,
+    left: `${props.left}%`
+  };
+
+  const start = moment(props.start);
+  const end = moment(props.end);
+
   return (
-    <dl className="DailyEvents-Appointment h-event" style={{ width: `${props.width}%`, height: `${props.height}%`, top: `${props.top}%`, left: `${props.left}%` }}>
-      <dt className="DailyEvents-Appointment-time"><time className="dt-start" dateTime="2013-06-30 18:00"></time>-<time className="dt-end" dateTime=""></time></dt>
+    <dl className="DailyEvents-Appointment h-event" style={ appointmentStyle }>
+      <dt className="DailyEvents-Appointment-time">
+        <time className="dt-start" dateTime={ start.format() }>{ start.format('HH:mm') }</time>
+        -
+        <time className="dt-end" dateTime={ end.format() }>{ end.format('HH:mm') }</time>
+      </dt>
       <dd className="DailyEvents-Appointment-content">
         <h1 className="DailyEvents-Appointment-title p-name">{ props.title }</h1>
-        <p className="DailyEvents-Appointment-description p-summary">Lorem ipsum dolor</p>
+        { props.description &&
+          <p className="DailyEvents-Appointment-description p-summary">{ props.description }</p>
+        }
       </dd>
     </dl>
   );
+};
+
+Appointment.propTypes = {
+  width: number.isRequired,
+  height: number.isRequired,
+  top: number.isRequired,
+  left: number.isRequired,
+  title: string.isRequired,
+  start: string.isRequired,
+  end: string.isRequired,
+  description: string
 };
 
 export default Appointment;
