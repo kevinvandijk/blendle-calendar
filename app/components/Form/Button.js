9405b1e219acac3364ec1f23bcc2fc5d95ada005
Kevin van Dijk <kevin@edgybits.com>
2017-02-06 15:28:17 +0100

    Add styling to Button components

--- app/components/Form/Button.js
+++ app/components/Form/Button.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { PropTypes } from '../../helpers';
 
-const Button = () => {
+const { string, oneOf } = PropTypes;
+
+const Button = (props) => {
+  let buttonClass = 'Form-Button';
+  if (props.type === 'primary') buttonClass = `${buttonClass} Form-Button--isPrimary`;
+
   return (
-    <button />
+    <button className={ buttonClass }>
+      { props.label }
+    </button>
   );
+};
+
+Button.propTypes = {
+  label: string.isRequired,
+  type: oneOf(['default', 'primary'])
+};
+
+Button.defaultProps = {
+  type: 'default'
 };
 
 export default Button;
