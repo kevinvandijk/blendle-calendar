9eadf3b32644d42fd2b0ffd197f23f97036523dd
Kevin van Dijk <kevin@edgybits.com>
2017-02-09 01:15:59 +0100

    Add support for localstorage, save state on every action

--- app/index.js
+++ app/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
 
 import reducer from './reducer';
 import Main from './components/Main';
 import './styles.scss';
 
+const engine = createEngine('blendle-calendar');
+const storageMiddleware = storage.createMiddleware(engine);
+const createStoreWithMiddleware = applyMiddleware(storageMiddleware)(createStore);
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducer,
+const store = createStoreWithMiddleware(
+  storage.reducer(reducer),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /* eslint-enable */
 
-ReactDOM.render(
-  <Provider store={ store }>
-    <Main />
-  </Provider>,
-  document.getElementById('root')
-);
+const load = storage.createLoader(engine);
+load(store).then(() => {
+  ReactDOM.render(
+    <Provider store={ store }>
+      <Main />
+    </Provider>,
+    document.getElementById('root')
+  );
+});
